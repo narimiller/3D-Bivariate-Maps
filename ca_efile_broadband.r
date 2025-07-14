765e4e3c13ed5da394996de35e50c6c4105e02ce
narimiller <narilifts@gmail.com>
2025-07-14 16:51:05 -0700

    edit color palette, recreate images

--- ca_efile_broadband.r
+++ ca_efile_broadband.r
@@ -39,6 +39,7 @@ irs <- read_csv("data/22incyallnoagi.csv") |>
   mutate(efile_rate = efiled / total_returns * 100)
 
 head(irs, 10)
+summary(irs$efile_rate)
 
 # fcc data
 fcc <- read_csv("data/county_tiers_201406_202406.csv") |>
@@ -59,6 +60,7 @@ fcc <- read_csv("data/county_tiers_201406_202406.csv") |>
   select(FIPS, broadband_pct)  # estimate % of households
 
 head(fcc, 10)
+summary(fcc$broadband_pct)
 
 # merge
 df <- left_join(irs, fcc, by = "FIPS") |> drop_na() |>
@@ -72,10 +74,10 @@ counties <- tigris::counties(cb = TRUE, class = "sf") |>
   mutate(FIPS = GEOID)
 
 # Define target CRS once
-target_crs <- "EPSG:5070"  # Albers Equal Area Conic projection (equal-area projection)
+target_crs <- "EPSG:5070"  # Albers Equal Area Conic projection
 counties_proj <- st_transform(counties, target_crs)
 
-# join with your data, gets sf object since sf comes first
+# join with data
 map_data <- left_join(counties_proj, df, by = "FIPS")
 
 # 4. BREAKS, PALETTE, AND THEME
@@ -83,34 +85,46 @@ map_data <- left_join(counties_proj, df, by = "FIPS")
 breaks <- bi_class( # info about bivariate scale and distribution
   map_data,
   x = efile_rate, y = broadband_pct,
-  style = "fisher", dim = 4
+  style = "fisher", dim = 3
 )
 
 # Define color palette
-pal <- "BlueYl"  
+custom_pal2 <- c(
+  "1-1" = "#a5e9cc", # low x, low y - lightest green
+  "2-1" = "#70ddaf", # medium x, low y 
+  "3-1" = "#26cf90", # high x, low y - most green
+  "1-2" = "#7fb8dd", # low x, medium y - light blue-green
+  "2-2" = "#56aebe", # medium x, medium y 
+  "3-2" = "#1da39c", # high x, medium y 
+  "1-3" = "#8773e4", # low x, high y - lightest purple
+  "2-3" = "#5c6dc4", # medium x, high y 
+  "3-3" = "#1f66a1"  # high x, high y - darkest blue-purple
+)
 
 # define custom theme for map
 custom_theme <- function(){
   theme_minimal() +
     theme(
-        axis.title = element_blank(),
-        axis.text = element_blank(),
-        plot.background = element_rect(
-            fill = "white", color = NA
-        ),
-        plot.title = element_text(
-            color = "grey10", hjust = 0.5,
-            face = "bold", vjust = -1 # move down towards map
-        ),
-        plot.subtitle = element_text(
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      plot.background = element_rect(
+        fill = "white", color = NA
+      ),
+      plot.title = element_text(
+        size = 16, 
+        color = "grey10", hjust = 0.5,
+        face = "bold", vjust = -1 # move down towards map
+      ),
+      plot.subtitle = element_text(
+        size = 12,
         hjust = 0.5, vjust = -1
-        ),
-        plot.caption = element_text(
-            size = 9, color = "grey20", 
-            hjust = 0.5, vjust = 1
-        ),
-        plot.margin = unit(c(0, 0, 0, 0), "lines"),
-        panel.grid = element_blank()
+      ),
+      plot.caption = element_text(
+        size = 9, color = "grey20", 
+        hjust = 0.5, vjust = 1
+      ),
+      plot.margin = unit(c(0, 0, 0, 0), "lines"),
+      panel.grid = element_blank()
     )
 }
 
@@ -118,18 +132,18 @@ custom_theme <- function(){
 # ------------------
 
 # create bivariate map using ggplot2
-map <- ggplot(breaks) + # define data
+map <- ggplot(breaks) +
   geom_sf(
     aes(
-      fill = bi_class # name of column needed for fill aesthetic
+      fill = bi_class # column for fill aesthetic
     ), show.legend = FALSE
   ) +
   biscale::bi_scale_fill( # how to fill values from bivariate scale
-      pal = pal, dim = 4,
-      flip_axes = TRUE,
-      rotate_pal = FALSE
+    pal = custom_pal2, dim = 3,
+    flip_axes = FALSE,
+    rotate_pal = FALSE
   ) +
-  labs( # labels
+  labs( 
       title = "CALIFORNIA: E-Filing Rate and Broadband Access by County",
       subtitle = "Electronic tax filing rates and estimated broadband access (≥25 Mbps), 2022",
       caption = "Source: IRS and FCC data, 2022",
@@ -140,12 +154,12 @@ map <- ggplot(breaks) + # define data
   ) +
   custom_theme()
 
-# create legend for bivariate map
+# create legend
 legend <- biscale::bi_legend(
-  pal = pal, 
-  flip_axes = TRUE,
+  pal = custom_pal2, 
+  flip_axes = FALSE,
   rotate_pal = FALSE,
-  dim = 4, 
+  dim = 3, 
   xlab = "E-file Rate (%)", 
   ylab = "Broadband Access (% of households)",
   size = 8.5 # size of label text in legend
@@ -159,8 +173,8 @@ x_range <- c(-2391586, -1611187)
 y_range <- c(1181840, 2513229)
 
 # Calculate total range
-x_total <- diff(x_range)  # 780399
-y_total <- diff(y_range)  # 1331389
+x_total <- diff(x_range)  
+y_total <- diff(y_range)  
 
 # Convert to 0-1 scale (like cowplot) (x=0.05, y=0.13, width=0.25, height=0.25)
 xmin <- x_range[1] + (0.62 * x_total)   
@@ -168,7 +182,7 @@ xmax <- x_range[1] + (1.00 * x_total)
 ymin <- y_range[1] + (0.54 * y_total)   
 ymax <- y_range[1] + (0.92 * y_total)   
 
-# map with legend
+# combine map with legend
 full_map <- map + 
   annotation_custom(
     grob = ggplotGrob(legend),
@@ -180,13 +194,12 @@ print(full_map)
 
 #save as PNG
 ggsave(
-  filename = "images/ca_efile_broadband_2d.png",
+  filename = "images/ca_efile_broadband_2d_2.png",
   width = 7, height = 10, dpi = 600,
   device = "png", bg = "white", full_map
 )
 
 # 5. CREATE TERRAIN LAYER
-# use terrain layer to create 3D effect
 # ------------------
 
 # Get California elevation data
@@ -197,7 +210,9 @@ ca_dem <- get_elev_raster(
 terra::rast() |> # convert to SpatRaster
 terra::mask(counties_proj)
 
-# project and convert to DEM to dataframe
+terra::crs(ca_dem)
+
+# project DEM and convert to dataframe
 dem_df <- ca_dem |>
   terra::project(target_crs) |>
   as.data.frame(xy = TRUE, na.rm = TRUE) # remove NAs
@@ -224,13 +239,13 @@ dem_map <- ggplot(
   custom_theme() +
   theme(legend.position = "none") # remove legend
 
-ggsave("images/check_dem_map.png", dem_map, width = 7, height = 10, dpi = 300)
+ggsave("images/check_dem_map_2.png", dem_map, width = 7, height = 10, dpi = 300)
 
 # 6. RENDER 3D SCENE
 # ------------------
 
 rayshader::plot_gg(
-  ggobj = full_map, # biv scale and legend
+  ggobj = full_map, # biv 2D map and legend
   ggobj_height = dem_map, # terrain layer
   width = 7, height = 10, # size of output in inches
   windowsize = c(700, 1000),
@@ -250,13 +265,13 @@ hdri_file <- basename(url)
 download.file(
   url = url,
   destfile = hdri_file,
-  mode = "wb" # write binary
+  mode = "wb" 
 )
 
 # 8. RENDER 3D OBJECT
 # ------------------
 rayshader::render_highquality(
-  filename = "images/ca_efile_broadband_3d.png",
+  filename = "images/ca_efile_broadband_3d_2.png",
   preview = TRUE,
   light = FALSE,
   environment_light = hdri_file,
